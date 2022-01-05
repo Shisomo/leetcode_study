54ad3c27c8fabf6922b17211167da73f374738d2
Shisomo <ysjyyyy@163.com>
2022-01-05 09:51:36 +0800

    20220105 commit for Offer study

--- Offer_30_Min_In_Stack/test.go
+++ Offer_30_Min_In_Stack/test.go
@@ -1,62 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type MinStack struct {
-	stack    []int
-	minStack []int
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-/** initialize your data structure here. */
-func Constructor() MinStack {
-	return MinStack{
-		stack:    []int{},
-		minStack: []int{math.MaxInt64},
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
 	}
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
 }
-
-func (this *MinStack) Push(x int) {
-	this.stack = append(this.stack, x)
-	if this.minStack[len(this.stack)-1] > x {
-		this.minStack = append(this.minStack, x)
-	} else {
-		this.minStack = append(this.minStack, this.minStack[len(this.minStack)-1])
-	}
-}
-
-func (this *MinStack) Pop() {
-	this.stack = this.stack[:len(this.stack)-1]
-	this.minStack = this.minStack[:len(this.minStack)-1]
-}
-
-func (this *MinStack) Top() int {
-	if this.stack != nil {
-		return this.stack[len(this.stack)-1]
-	}
-	return -1
-}
-
-func (this *MinStack) Min() int {
-	return this.minStack[len(this.minStack)-1]
-}
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Push(x);
- * obj.Pop();
- * param_3 := obj.Top();
- * param_4 := obj.Min();
- */
 func main() {
-	obj := Constructor()
-	obj.Push(1)
-	obj.Pop()
-	fmt.Println(obj.Min())
-	fmt.Println(obj.Top())
+	var obj ListNode
+	obj.Val = 21
+	fmt.Println(obj.Val)
 }
 
 /**
