6ef153bff9b137d65818cf6f1a91bd719caeb34c
3900X <shisomoy@gmail.com>
2022-01-28 18:02:28 +0800

    20220128 commit

---
+++ 0067_Add_Binary/test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	testdata1 := "1011"
+	result := addBinary(testdata1, testdata1)
+	fmt.Printf("result is : %s", result)
+}
+
+func addBinary(a string, b string) string {
+	ans := ""
+	carry := 0
+	lenA, lenB := len(a), len(b)
+	n := max(lenA, lenB)
+	for i := 0; i < n; i++ {
+		if i < lenA {
+			carry += int(a[lenA-i-1] - '0')
+		}
+		if i < lenB {
+			carry += int(b[lenB-i-1] - '0')
+		}
+		ans = strconv.Itoa(carry%2) + ans
+		carry /= 2
+	}
+	if carry > 0 {
+		ans = "1" + ans
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
